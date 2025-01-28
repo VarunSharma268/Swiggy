95cabd790b61d07e5fb31e6a82c235037f622782
VarunSharma268 <varunspharma666@gmail.com>
2025-01-28 13:55:05 +0530

    Add files via upload

---
+++ data/hotels.js
@@ -0,0 +1,352 @@
+export default [
+    {
+        id:"0",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGMn83-WFtplI33cmhOiY5JpOykL1fufKs7Q&s",
+        name:"Hungry Point",
+        rating:4.3,
+        time:"20-25",
+        address:"Near Himani Hotel, Solan",
+        ratings:"1k",
+        cost_for_two:250,
+        cuisines:"North Indian, South Indian",
+        menu:[
+            {
+                id:"20",
+                name:"Recommended",
+                items:[
+                    {
+                        id:"101",
+                        name:"Spicy Paneer Pizza",
+                        price:200,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.1,
+                        ratings:43,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGMn83-WFtplI33cmhOiY5JpOykL1fufKs7Q&s",
+                        veg:true,
+                        bestSeller:false,
+                        quantity:1
+                    },
+                    {
+                        id:"102",
+                        name:"Pizza Margherita",
+                        price:250,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.3,
+                        ratings:34,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_5tuQu8Pl9Z4sRdg-ZBVA4jaqQhL4AJMkyg&s",
+                        veg:true,
+                        bestSeller:true,
+                        quantity:1
+                    },
+                    {
+                        id:"103",
+                        name:"Capsicum Pizza",
+                        price:100,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.5,
+                        ratings:56,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1N9OTA8gFMTon46rosjuWvJXOY8qMDcA3og&s",
+                        veg:true,
+                        bestSeller:false,
+                        quantity:1
+                    },
+                    {
+                        id:"104",
+                        name:"Tomato Pizza",
+                        price:100,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:3.8,
+                        ratings:22,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWPQVRrJUz6Ct7OmXTexvLDN9GGq3zeY4M5g&s",
+                        veg:true,
+                        bestSeller:true,
+                        quantity:1
+                    },
+                    {
+                        id:"105",
+                        name:"Corn Pizza",
+                        price:99,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.5,
+                        ratings:45,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEninsEuvgqwJ4OPNQv8qx1S2lwZRWCzMBXg&s",
+                        veg:true,
+                        bestSeller:true,
+                        quantity:1
+                    },
+                ]
+            },
+           
+]
+},
+{
+        id:"1",
+        image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/oorc4tldeqqtcc1yttzn",
+        name:"Aao Ji Te Khao Ji",
+        rating:4.5,
+        time:"30-40",
+        address:"Mall Road, Solan",
+        ratings:"1k",
+        cost_for_two:150,
+        cuisines:"north Indian, South Indian",
+        menu:[
+            {
+                id:"21",
+                name:"Recommended",
+                items:[
+                    {
+                        id:"201",
+                        name:"Gol Gappe",
+                        price:35,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.1,
+                        ratings:43,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgvG3dDHA85mgQ7G5LhNh0a4OYxONswgUMpw&s",
+                        veg:true,
+                        bestSeller:false,
+                        quantity:1
+                    },
+                    {
+                        id:"202",
+                        name:"Vada Pav",
+                        price:40,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.3,
+                        ratings:34,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn6-bdZcFpcUnh-UjyUFfm-lnLhn13cLs_2g&s",
+                        veg:true,
+                        bestSeller:true,
+                        quantity:1
+                    },
+                    {
+                        id:"203",
+                        name:"Spring Rolls ",
+                        price:100,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.5,
+                        ratings:56,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7ECfxuN-rJCRdr5KM46e8Tbo1vxw7V9J_vQ&s",
+                        veg:true,
+                        bestSeller:false,
+                        quantity:1
+                    },
+                    {
+                        id:"204",
+                        name:"Pav Bhaji",
+                        price:70,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:3.8,
+                        ratings:22,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbKOLPyK52CxsfpfTdLzMCzzq9Oq_Lnne7QA&s",
+                        veg:true,
+                        bestSeller:true,
+                        quantity:1
+                    },
+                    {
+                        id:"205",
+                        name:"Aloo Tikki",
+                        price:40,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.5,
+                        ratings:45,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLnTpV38EOdb5l9MOgLHYOJHCDMQgvu3Rjmg&s",
+                        veg:true,
+                        bestSeller:true,
+                        quantity:1
+                    },
+                ]
+            },
+            {
+                id:"12",
+                name:"Special Thali",
+                items:[
+                    {
+                        id:"206",
+                        name:"Rajma Chawal",
+                        price:50,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.6,
+                        ratings:56,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2rlrDTw72GwwcJdmTZZi6El0K9qgyScpcUg&s",
+                        veg:true,
+                        bestSeller:true,
+                    },
+                    {
+                        id:"207",
+                        name:" Kari Chawal",
+                        price:50,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.3,
+                        ratings:34,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThBrDVlMpmAncFCIrkAP9tXMyOtQbtd5gtOA&s",
+                        veg:true,
+                        bestSeller:true,
+                        
+                    },
+                    {
+                        id:"208",
+                        name:"Matar Paneer Chawal",
+                        price:60,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.3,
+                        ratings:52,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSag_FV83_NIc7IOEuNCVziMWTAph04dfYkSA&s",
+                        veg:true,
+                        bestSeller:false,
+                        
+                    },
+                    {
+                        id:"209",
+                        name:"Sahi Paneer Chawal",
+                        price:80,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.5,
+                        ratings:48,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdIfOt8qtsAVU-Py1kjJJRPP1byxkMAcurHQ&s",
+                        veg:true,
+                        bestSeller:true,
+                        
+                    },
+        ]
+        
+        
+    }
+]
+
+},
+{
+        id:"2",
+        image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/3/255fee43-aecd-40f9-9df3-c5380b256193_920343.jpg",
+        name:"Chilli Chatkara",
+        rating:4.0,
+        time:"10-15",
+        address:"Solan Town",
+        ratings:"1k",
+        cost_for_two:250,
+        cuisines:"north Indian, South Indian",
+        menu:[
+            {
+                id:"22",
+                name:"Recommended",
+                items:[
+                    {
+                        id:"301",
+                        name:"Dahi Balla",
+                        price:95,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.1,
+                        ratings:43,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXKaq3hpKLFgbmiy6-a5SJgzanl6lKnNktFQ&s",
+                        veg:true,
+                        bestSeller:false,
+                        quantity:1
+                    },
+                    {
+                        id:"302",
+                        name:" Gol Gappe",
+                        price:70,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.3,
+                        ratings:34,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgvG3dDHA85mgQ7G5LhNh0a4OYxONswgUMpw&s",
+                        veg:true,
+                        bestSeller:true,
+                        quantity:1
+                    },
+                    {
+                        id:"303",
+                        name:"Desi Ghee Chilla",
+                        price:100,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.5,
+                        ratings:56,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyM3mjtXqXB2Yp6Bie2f2cDiTGKnq5d_xbKw&s",
+                        veg:true,
+                        bestSeller:false,
+                        quantity:1
+                    },
+                    {
+                        id:"304",
+                        name:"Bombai Bhelpuri",
+                        price:70,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:3.8,
+                        ratings:22,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVl2QhXMr1z0oxUQR0r0_JnC-H8jhmoV2tHg&s",
+                        veg:true,
+                        bestSeller:true,
+                        quantity:1
+                    },
+                    {
+                        id:"305",
+                        name:"Indori Sev Puri",
+                        price:80,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.5,
+                        ratings:45,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdTDoAgniOFNr5dAnKM4fzjz52hWddGg69cg&s",
+                        veg:true,
+                        bestSeller:true,
+                        quantity:1
+                    },
+                ]
+            },
+            {
+                id:"13",
+                name:"Special thali",
+                items:[
+                    {
+                        id:"306",
+                        name:"Special Chole Bhature",
+                        price:129,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.6,
+                        ratings:56,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGnwXb0_Na7S0tyE7VlZ_olh1H7tTqIxI7eg&s",
+                        veg:true,
+                        bestSeller:true,
+                    },
+                    {
+                        id:"307",
+                        name:"Dal Makhani",
+                        price:190,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.3,
+                        ratings:34,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHl-C30YNh24cBNrzNMYuXvoxzFKR8fYOpfA&s",
+                        veg:true,
+                        bestSeller:true,
+                        
+                    },
+                    {
+                        id:"308",
+                        name:"Chole Chawal",
+                        price:90,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.3,
+                        ratings:52,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjhCO0Eoxn3qs2dsMkZWkf2q30oLMOmu6M3Q&s",
+                        veg:true,
+                        bestSeller:false,
+                        
+                    },
+                    {
+                        id:"309",
+                        name:"Rajma Chawal",
+                        price:95,
+                        description:"E: 723.82 KCal (241.27 KCal), C:30.89 Grams (10.30 Grams), P: 32.95 Grams (10.98 Grams), F: 51.82 Grams (17.27 Grams)",
+                        rating:4.5,
+                        ratings:48,
+                        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2rlrDTw72GwwcJdmTZZi6El0K9qgyScpcUg&s",
+                        veg:true,
+                        bestSeller:true,
+                        
+                    },
+        ]
+        
+        
+    }
+]
+
+},
+]
---
+++ data/quickfood.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        id:"0",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnF6GcEiB57mF8qkQJHDLFRGebdaIOV4Y5IQ&s",
+        name:"California Burrito",
+        rating:4.5,
+        time:"20-30",
+        offer:"30%",
+    },
+    {
+        id:"1",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXpkYjg2FcWdraavExFoe4IgN4sjqPGziH1w&s",
+        name:"McDonald's",
+        rating:4.3,
+        time:"30-40",
+        offer:"50%",
+    },
+    {
+        id:"2",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTDtfO3Q-BTgjA7iDxyJNQX_A0Q6JLjEj9AQ&s",
+        name:"Momos",
+        rating:4.5,
+        time:"30-40",
+        offer:"40%",
+    },
+    {
+        id:"3",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXmle_4GwQ6PmRee--U_LQVt_qLGeZ_s_nbA&s",
+        name:"Special Biryani",
+        rating:3.8,
+        time:"25-35",
+        offer:"60%",
+    },
+    {
+        id:"4",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlsS67wVi2Odpq8fVUOOdUHWFSMbXAj4IAEw&s",
+        name:"Hyderabadi Bawarchi",
+        rating:4.1,
+        time:"30-40",
+        offer:"55%",
+    },
+]
